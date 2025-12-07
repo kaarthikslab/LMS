a0e562afb499b6d3ac370f60dd46ccc1c335111c
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 18:55:55 +0530

    Update IssuedDetails.js

--- src/components/IssuedDetails.js
+++ src/components/IssuedDetails.js
@@ -8,28 +8,6 @@ const IssuedDetails = () => {
   const totalExpenditure = book.issued.reduce((sum, i) => sum + (book.price * (new Date(i.returnedDate || new Date()) - new Date(i.issuedDate)) / (1000 * 60 * 60 * 24)), 0);
 
   return (
-    <div style={{ marginTop: '100px' }}> {/* 5-line gap below header */}
-      <div className="glass" style={{ position: 'absolute', top: '80px', right: '20px', padding: '10px' }}>
-        <h3>Total Expenditure: <span className="accent">${totalExpenditure.toFixed(2)}</span></h3>
-      </div>
-      <div className="glass" style={{ padding: '20px', overflowY: 'auto', height: '400px' }}>
-        <table style={{ width: '100%', color: 'white' }}>
-          <thead>
-            <tr>
-              <th>Name</th><th>Issued Date</th><th>Due Date</th><th>Returned Date</th><th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {book.issued.map((i, idx) => (
-              <tr key={idx}>
-                <td>{i.name}</td><td>{i.issuedDate}</td><td>{i.dueDate}</td><td>{i.returnedDate || 'N/A'}</td><td>{i.status}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
-
-export default IssuedDetails;
+    <div className="page-bg" style={{ marginTop: '80px' }}>
+      <div className="glass" style={{ position: 'fixed', top: '80px', right: '20px', padding: '10px', zIndex: 1000 }}>
+        <h3>Total
