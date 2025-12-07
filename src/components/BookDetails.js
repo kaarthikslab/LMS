dfe6083d86924480fd187407c233c4a3d25be454
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 22:25:26 +0530

    Update BookDetails.js

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -13,9 +13,4 @@ const BookDetails = () => {
       <p>Price: <span className="accent">${book.price}</span></p>
       <p>Stock: {book.stock}</p>
       <label>Status: <input type="checkbox" checked={status === 'available'} onChange={() => setStatus(status === 'available' ? 'issued' : 'available')} /></label>
-      <button className="neu-button" style={{ marginTop: '10px' }}>Restock Book</button>
-    </div>
-  );
-};
-
-export default BookDetails;
+      <button className="neu-button" style={{ marginTop: '
