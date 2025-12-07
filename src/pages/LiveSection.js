c6658372f693c588dd83cdd46e42fa95688dde86
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 18:26:09 +0530

    Update LiveSection.js

--- src/pages/LiveSection.js
+++ src/pages/LiveSection.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import BookTile from '../components/BookTile';
-import { mockBooks } from '../data/mockdata';
+import { mockBooks } from '../data/mockData';
 
 const LiveSection = () => (
-  <div style={{ marginTop: '100px' }}>
+  <div style={{ marginTop: '100px' }}> {/* 5-line gap below header */}
     <h2 className="primary">Live Books</h2>
     <div className="grid">
       {mockBooks.filter(b => b.status === 'available').map(book => <BookTile key={book.id} book={book} type="book" />)}
