23545fae615be666799875a93f962e792edc4b88
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 17:26:23 +0530

    Update LiveSection.js

--- src/pages/LiveSection.js
+++ src/pages/LiveSection.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import BookTile from '../components/BookTile';
+import { mockBooks } from '../data/mockData';
 
+const LiveSection = () => (
+  <div className="grid">
+    {mockBooks.filter(b => b.status === 'available').map(book => <BookTile key={book.id} book={book} type="book" />)}
+  </div>
+);
+
+export default LiveSection;
