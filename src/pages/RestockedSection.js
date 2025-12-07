936c06f0baf0fc83f5fd130414996f0ce0e567f3
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 18:55:12 +0530

    Update RestockedSection.js

--- src/pages/RestockedSection.js
+++ src/pages/RestockedSection.js
@@ -3,8 +3,8 @@ import BookTile from '../components/BookTile';
 import { mockBooks } from '../data/mockData';
 
 const RestockedSection = () => (
-  <div style={{ marginTop: '100px' }}> {/* 5-line gap below header */}
-    <h2 className="primary">Restocked Books</h2>
+  <div className="page-bg" style={{ marginTop: '80px' }}>
+    <h2 className="accent">Restocked Books</h2>
     <div className="grid">
       {mockBooks.filter(b => b.restocked.length > 0).map(book => <BookTile key={book.id} book={book} type="restocked" />)}
     </div>
