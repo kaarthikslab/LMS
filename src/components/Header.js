92d86b5603387899e77dd8c2660d9b96e79f89b8
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 17:24:18 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
+const Header = () => (
+  <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+    <div style={{ marginTop: '40px' }}> {/* 2 lines below top */}
+      <h1 className="primary">Library Management</h1>
+    </div>
+    <div>
+      <Link to="/live" className="accent">Live Books</Link> | 
+      <Link to="/issued" className="accent">Issued</Link> | 
+      <Link to="/restocked" className="accent">Restocked</Link>
+    </div>
+    <SearchBar />
+  </header>
+);
+
+export default Header;
