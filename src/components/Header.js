c5126b27f1a1420c87fe12b1519d072f3225d485
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 18:12:13 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from './SearchBar';
+import SearchBar from './Searchbar';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
