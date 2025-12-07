c07f0942fca21db990ae9ed2cac7b6de47bf44c1
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 17:26:05 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import KPICards from '../components/KPICards';
 
+const Dashboard = () => (
+  <div>
+    <KPICards />
+    <div className="glass" style={{ height: '300px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      {/* Placeholder for 3 images */}
+      <div>Image 1</div>
+      <div>Image 2</div>
+      <div>Image 3</div>
+    </div>
+  </div>
+);
+
+export default Dashboard;
