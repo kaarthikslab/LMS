f2d389422fd58515aad463adad58d9d25fa56586
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 17:24:36 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Navigation = () => (
+  <nav className="glass" style={{ width: '200px', height: '100vh', padding: '20px', position: 'fixed' }}>
+    <ul style={{ listStyle: 'none', padding: 0 }}>
+      <li><Link to="/" className="primary">Dashboard</Link></li>
+      <li><Link to="/live">Live Section</Link></li>
+      <li><Link to="/issued">Issued Section</Link></li>
+      <li><Link to="/restocked">Restocked Section</Link></li>
+      <li><Link to="/settings">Settings</Link></li>
+    </ul>
+  </nav>
+);
+
+export default Navigation;
