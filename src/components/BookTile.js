82808209034f9565ed9e49eeda91d7b402ed04fa
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 17:25:18 +0530

    Update BookTile.js

--- src/components/BookTile.js
+++ src/components/BookTile.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const BookTile = ({ book, type }) => (
+  <Link to={`/${type}/${book.id}`} className="glass square-tile">
+    <h3>{book.name}</h3>
+    <p>{book.code}</p>
+    <small>{book.publisher}</small>
+  </Link>
+);
+
+export default BookTile;
