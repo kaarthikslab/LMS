23fe05fb916665183fd7f112a016785376dfa938
kaarthikslab <kaarthikscareer@gmail.com>
2025-12-07 18:54:39 +0530

    Update IssuedSection.js

--- src/pages/IssuedSection.js
+++ src/pages/IssuedSection.js
@@ -3,8 +3,8 @@ import BookTile from '../components/BookTile';
 import { mockBooks } from '../data/mockData';
 
 const IssuedSection = () => (
-  <div style={{ marginTop: '100px' }}> {/* 5-line gap below header */}
-    <h2 className="primary">Issued Books</h2>
+  <div className="page-bg" style={{ marginTop: '80px' }}>
+    <h2 className="accent">Issued Books</h2>
     <div className="grid">
       {mockBooks.filter(b => b.status === 'issued').map(book => <BookTile key={book.id} book={book} type="issued" />)}
     </div>
